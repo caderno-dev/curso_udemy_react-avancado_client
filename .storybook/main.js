7c2bb20d0ac0a5634ec4bd7445c7adb7deacc0b4
Felipe B. Barbosa <felipebbarbosa@gmail.com>
2023-07-19 18:46:33 -0300

    chore: setup absolute path of Storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,15 @@
 module.exports = {
-  "stories": ["../src/components/**/stories.@(js|jsx|ts|tsx)"],
-  "addons": ["@storybook/addon-essentials"],
-  "framework": {
+  stories: ["../src/components/**/stories.@(js|jsx|ts|tsx)"],
+  addons: ["@storybook/addon-essentials"],
+  framework: {
     name: "@storybook/nextjs",
     options: {}
   },
   docs: {
     autodocs: true
+  },
+  webpackFinal: (config) => {
+    config.resolve.modules.push(`${process.cwd()}/src`)
+    return config
   }
 };
