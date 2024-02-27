e4690c6f6c40e1a6ac9f4f684dac660fe3a3c132
Felipe B. Barbosa <felipebbarbosa@gmail.com>
2024-02-26 22:32:44 -0300

    chore: update stories of Main component

--- src/components/Main/stories.tsx
+++ src/components/Main/stories.tsx
@@ -1,4 +1,4 @@
-import { Story, Meta } from '@storybook/react/types-6-0'
+import { StoryFn, Meta } from '@storybook/react'
 import Main from '.'
 
 export default {
@@ -10,9 +10,9 @@ export default {
   }
 } as Meta
 
-export const Basic: Story = (args) => <Main {...args} />
+export const Basic: StoryFn = (args) => <Main {...args} />
 Basic.args = {
   title: 'Título básico'
 }
 
-export const Default: Story = (args) => <Main {...args} />
+export const Default: StoryFn = (args) => <Main {...args} />
