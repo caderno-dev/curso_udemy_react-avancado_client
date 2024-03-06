3601a8b2aa95507d2415714d25111231b33c1767
Felipe B. Barbosa <felipebbarbosa@gmail.com>
2024-03-05 22:47:31 -0300

    chore: add Home styles and tests

--- src/templates/Home/index.tsx
+++ src/templates/Home/index.tsx
@@ -36,44 +36,52 @@ const Home = ({
   <section>
     <Container>
       <Menu />
-      <BannerSlider items={banners} />
+      <S.SectionBanner>
+        <BannerSlider items={banners} />
+      </S.SectionBanner>
     </Container>
 
-    <Container>
-      <Heading lineLeft lineColor="secondary" color="black">
-        News
-      </Heading>
-      <GameCardSlider items={newGames} color="black" />
-    </Container>
+    <S.SectionNews>
+      <Container>
+        <Heading lineLeft lineColor="secondary" color="black">
+          News
+        </Heading>
+        <GameCardSlider items={newGames} color="black" />
+      </Container>
+    </S.SectionNews>
 
     <Container>
-      <Heading lineLeft lineColor="secondary">
-        Most Popular
-      </Heading>
-      <Highlight {...mostPopularHighlight} />
-      <GameCardSlider items={mostPopularGames} />
+      <S.SectionMostPopular>
+        <Heading lineLeft lineColor="secondary">
+          Most Popular
+        </Heading>
+        <Highlight {...mostPopularHighlight} />
+        <GameCardSlider items={mostPopularGames} />
+      </S.SectionMostPopular>
     </Container>
 
-    <Container>
+    <S.SectionUpcoming>
       <Heading lineLeft lineColor="secondary">
         Upcomming
       </Heading>
       <GameCardSlider items={upcommingGames} />
       <Highlight {...upcommingHighlight} />
       <GameCardSlider items={upcommingMoreGames} />
-    </Container>
+    </S.SectionUpcoming>
 
-    <Container>
+    <S.SectionFreeGames>
       <Heading lineLeft lineColor="secondary">
         Free Games
       </Heading>
       <Highlight {...freeHighlight} />
       <GameCardSlider items={freeGames} />
-    </Container>
+    </S.SectionFreeGames>
 
-    <Container>
-      <Footer />
-    </Container>
+    <S.SectionFooter>
+      <Container>
+        <Footer />
+      </Container>
+    </S.SectionFooter>
   </section>
 )
 
