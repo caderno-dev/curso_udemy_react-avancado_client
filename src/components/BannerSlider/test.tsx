05406843dafa11007ec794270d03cdb201f84e7f
Felipe B. Barbosa <felipebbarbosa@gmail.com>
2024-03-05 22:24:59 -0300

    chore: create mocks to BannerSlider, GameCardSlider and Highlight components

--- src/components/BannerSlider/test.tsx
+++ src/components/BannerSlider/test.tsx
@@ -4,23 +4,7 @@ import { renderWithTheme } from 'utils/tests/helpers'
 
 import BannerSlider from '.'
 
-const items = [
-  {
-    img: 'https://source.unsplash.com/user/willianjusten/1042x580',
-    title: 'Defy death 1',
-    subtitle: '<p>Play the new <strong>CrashLands</strong> season',
-    buttonLabel: 'Buy now',
-    buttonLink: '/games/defy-death',
-    ribbon: 'Bestselling'
-  },
-  {
-    img: 'https://source.unsplash.com/user/willianjusten/1042x582',
-    title: 'Defy death 2',
-    subtitle: '<p>Play the new <strong>CrashLands</strong> season',
-    buttonLabel: 'Buy now',
-    buttonLink: '/games/defy-death'
-  }
-]
+import items from './mock'
 
 describe('<BannerSlider />', () => {
   it('should render vertical slider', () => {
--- src/components/GameCardSlider/stories.tsx
+++ src/components/GameCardSlider/stories.tsx
@@ -3,50 +3,7 @@ import { GameCardProps } from 'components/GameCard'
 
 import GameCardSlider from '.'
 
-const items = [
-  {
-    title: 'Population Zero',
-    developer: 'Rockstar Games',
-    img: 'https://source.unsplash.com/user/willianjusten/300x140',
-    price: 'R$ 235,00',
-    promotionalPrice: 'R$ 215,00'
-  },
-  {
-    title: 'Population Zero',
-    developer: 'Rockstar Games',
-    img: 'https://source.unsplash.com/user/willianjusten/300x141',
-    price: 'R$ 235,00',
-    promotionalPrice: 'R$ 215,00'
-  },
-  {
-    title: 'Population Zero',
-    developer: 'Rockstar Games',
-    img: 'https://source.unsplash.com/user/willianjusten/300x142',
-    price: 'R$ 235,00',
-    promotionalPrice: 'R$ 215,00'
-  },
-  {
-    title: 'Population Zero',
-    developer: 'Rockstar Games',
-    img: 'https://source.unsplash.com/user/willianjusten/300x143',
-    price: 'R$ 235,00',
-    promotionalPrice: 'R$ 215,00'
-  },
-  {
-    title: 'Population Zero',
-    developer: 'Rockstar Games',
-    img: 'https://source.unsplash.com/user/willianjusten/300x144',
-    price: 'R$ 235,00',
-    promotionalPrice: 'R$ 215,00'
-  },
-  {
-    title: 'Population Zero',
-    developer: 'Rockstar Games',
-    img: 'https://source.unsplash.com/user/willianjusten/300x145',
-    price: 'R$ 235,00',
-    promotionalPrice: 'R$ 215,00'
-  }
-]
+import items from './mock'
 
 export default {
   title: 'GameCardSlider',
@@ -58,12 +15,6 @@ export default {
     }
   }
 } as Meta
-
-//export const Default: StoryObj<GameCardProps[]> = (args) => (
-//  <div style={{ maxWidth: '130rem', margin: '0 auto' }}>
-//    <GameCardSlider items={args} {...args} />
-//  </div>
-//)
 
 export const Default = {
   render: (args: GameCardProps[]) => (
