9d63a60057d6e95ec35571d6f35b0665943bbc98
Felipe B. Barbosa <felipebbarbosa@gmail.com>
2024-04-13 06:49:05 -0300

    chore: organize storybook Form items

--- src/components/Radio/stories.tsx
+++ src/components/Radio/stories.tsx
@@ -2,7 +2,7 @@ import { StoryFn, Meta } from '@storybook/react'
 import Radio, { RadioProps } from '.'
 
 export default {
-  title: 'Radio',
+  title: 'Form/Radio',
   component: Radio,
   parameters: {
     layout: 'fullscreen',
