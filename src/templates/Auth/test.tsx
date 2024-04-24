b9b0aed3c00a44492720944d787cd9534f1e98b2
Felipe B. Barbosa <felipebbarbosa@gmail.com>
2024-04-24 06:18:39 -0300

    chore: add Auth initial structure

---
+++ src/templates/Auth/test.tsx
@@ -0,0 +1,13 @@
+import { render, screen } from '@testing-library/react'
+
+import Auth from '.'
+
+describe('<Auth />', () => {
+  it('should render all components and children', () => {
+    render(
+      <Auth title="Auth Title">
+        <input type="text" />
+      </Auth>
+    )
+  })
+})
