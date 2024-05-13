b7a58ab3d76f44ec7bbcfd15e50b1bc06eafa14e
Felipe B. Barbosa <felipebbarbosa@gmail.com>
2024-05-13 06:51:32 -0300

    chore: add GameInfo styles, mock and story

--- src/components/GameInfo/stories.tsx
+++ src/components/GameInfo/stories.tsx
@@ -1,9 +1,16 @@
 import { StoryFn, Meta } from '@storybook/react'
-import GameInfo from '.'
+import GameInfo, { GameInfoProps } from '.'
+
+import mockGame from './mock'
 
 export default {
   title: 'GameInfo',
-  component: GameInfo
+  component: GameInfo,
+  args: mockGame
 } as Meta
 
-export const Basic: StoryFn = () => <GameInfo />
+export const Basic: StoryFn<GameInfoProps> = (args) => (
+  <div style={{ maxWidth: '144rem', margin: 'auto', padding: '1.5rem' }}>
+    <GameInfo {...args} />
+  </div>
+)
