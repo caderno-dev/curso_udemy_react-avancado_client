eb3144e31f2ed0ae7ed609ed192c48ea6847ecf0
Felipe B. Barbosa <felipebbarbosa@gmail.com>
2024-05-18 16:24:03 -0300

    chore: add GameDetails initial structure

---
+++ src/components/GameDetails/stories.tsx
@@ -0,0 +1,13 @@
+import { StoryFn, Meta } from '@storybook/react'
+import GameDetails from '.'
+
+export default {
+  title: 'GameDetails',
+  component: GameDetails
+} as Meta
+
+export const Basic: StoryFn = () => (
+  <div style={{ maxWidth: '130rem', margin: '0 auto' }}>
+    <GameDetails />
+  </div>
+)
