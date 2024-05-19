163ef3bc54ef91bf920941e3471e31e1f751d476
Felipe B. Barbosa <felipebbarbosa@gmail.com>
2024-05-19 05:50:28 -0300

    chore: add GameDetails storybook details and adjust another ones

--- src/components/GameDetails/stories.tsx
+++ src/components/GameDetails/stories.tsx
@@ -1,17 +1,30 @@
 import { StoryFn, Meta } from '@storybook/react'
 import GameDetails, { GameDetailsProps } from '.'
+import mockGame from './mock'
 
 export default {
   title: 'GameDetails',
   component: GameDetails,
-  args: {
-    platforms: ['windows', 'linux', 'mac']
+  parameters: {
+    backgrounds: {
+      default: 'dark'
+    }
   },
+  args: mockGame,
   argTypes: {
+    releaseDate: {
+      control: 'date'
+    },
     platforms: {
       control: {
         type: 'inline-check',
         options: ['windows', 'linux', 'mac']
+      }
+    },
+    genres: {
+      control: {
+        type: 'inline-check',
+        options: ['Role-playing', 'Narrative']
       }
     }
   }
