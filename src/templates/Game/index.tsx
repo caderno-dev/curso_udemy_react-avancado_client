15e5c14f150632aa21268797d914f3faf98e78d4
Felipe Barbosa <felipebbarbosa@gmail.com>
2024-08-17 07:26:50 -0300

    chore: add Divider to Wishlist and Game page

--- src/templates/Game/index.tsx
+++ src/templates/Game/index.tsx
@@ -3,6 +3,7 @@ import GameInfo, { GameInfoProps } from 'components/GameInfo'
 import Gallery, { GalleryImageProps } from 'components/Gallery'
 import GameDetails, { GameDetailsProps } from 'components/GameDetails'
 import TextContent from 'components/TextContent'
+import { Divider } from 'components/Divider'
 import { GameCardProps } from 'components/GameCard'
 import { HighlightProps } from 'components/Highlight'
 import Showcase from 'components/Showcase'
@@ -48,6 +49,7 @@ const Game = ({
 
       <S.SectionGameDetails>
         <GameDetails {...details} />
+        <Divider />
       </S.SectionGameDetails>
 
       <Showcase
