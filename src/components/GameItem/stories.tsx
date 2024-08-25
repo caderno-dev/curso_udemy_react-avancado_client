5ce46094b98001a24a27f59f98b8498aeb08e86b
Felipe Barbosa <felipebbarbosa@gmail.com>
2024-08-25 18:12:01 -0300

    chore: add PaymentInfo to GameItem

--- src/components/GameItem/stories.tsx
+++ src/components/GameItem/stories.tsx
@@ -18,5 +18,11 @@ export const WithPayment: StoryFn<GameItemProps> = (args) => (
 )
 
 WithPayment.args = {
-  downloadLink: 'https://wongames.com/game/download/2323213'
+  downloadLink: 'https://wongames.com/game/download/2323213',
+  paymentInfo: {
+    flag: 'mastercard',
+    img: '/img/master-card.png',
+    number: '**** **** **** 4326',
+    purchaseDate: 'Purchase made on 07/20/2020 at 20:32'
+  }
 }
