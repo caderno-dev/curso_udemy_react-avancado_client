f72b150dddecaf1b12a7045a642e1044e7adbe06
Felipe Barbosa <felipebbarbosa@gmail.com>
2024-08-25 17:52:24 -0300

    chore: create GameItem initial structure

---
+++ src/components/GameItem/stories.tsx
@@ -0,0 +1,14 @@
+import { StoryFn, Meta } from '@storybook/react'
+import GameItem, { GameItemProps } from '.'
+
+export default {
+  title: 'GameItem',
+  component: GameItem,
+  args: {
+    img: 'https://source.unsplash.com/user/willianjusten/151x70',
+    title: 'Red Dead Redemption 2',
+    price: 'R$ 215,00'
+  }
+} as Meta
+
+export const Basic: StoryFn<GameItemProps> = (args) => <GameItem {...args} />
