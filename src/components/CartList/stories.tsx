d577b1443ea4b04cb434adb6649c9598a3eaf246
Felipe Barbosa <felipebbarbosa@gmail.com>
2024-08-26 06:31:59 -0300

    chore: create CartList

---
+++ src/components/CartList/stories.tsx
@@ -0,0 +1,29 @@
+import { StoryFn, Meta } from '@storybook/react'
+import CartList, { CartListProps } from '.'
+
+import mockItems from './mock'
+
+export default {
+  title: 'CartList',
+  component: CartList,
+  args: {
+    items: mockItems,
+    total: 'R$ 330,00'
+  },
+  argTypes: {
+    items: {
+      type: 'symbol'
+    }
+  },
+  parameters: {
+    backgrounds: {
+      default: 'dark'
+    }
+  }
+} as Meta
+
+export const Basic: StoryFn<CartListProps> = (args) => (
+  <div style={{ maxWidth: 800 }}>
+    <CartList {...args} />
+  </div>
+)
