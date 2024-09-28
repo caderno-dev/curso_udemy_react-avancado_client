4da7c9f63af6a5a985e825eb992c4641cacb4c15
Felipe Barbosa <felipebbarbosa@gmail.com>
2024-09-28 07:09:00 -0300

    chore: add CartDropdown

---
+++ src/components/CartDropdown/stories.tsx
@@ -0,0 +1,24 @@
+import { StoryFn, Meta } from '@storybook/react'
+import CartDropdown, { CartDropdownProps } from '.'
+
+import items from 'components/CartList/mock'
+
+export default {
+  title: 'CartDropdown',
+  component: CartDropdown,
+  args: {
+    items,
+    total: 'R$ 300,00'
+  },
+  parameters: {
+    backgrounds: {
+      default: 'dark'
+    }
+  }
+} as Meta
+
+export const Default: StoryFn<CartDropdownProps> = (args) => (
+  <div style={{ maxWidth: '98%', display: 'flex', justifyContent: 'flex-end' }}>
+    <CartDropdown {...args} />
+  </div>
+)
